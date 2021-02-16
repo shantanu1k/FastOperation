0af94d834f6ce1ebb6006e9fd437c661bf2757e2
shantanu1k <57897297+shantanu1k@users.noreply.github.com>
2021-02-16 18:40:20 +0530

    Added Decimal to Binary

--- lib/FastOperation.dart
+++ lib/FastOperation.dart
@@ -26,4 +26,46 @@ class FastOperation {
   }
   return octal.toInt();
  }
+ String reverseString(String input) {
+    var chars = input.split('');
+    return chars.reversed.join();
+  }
+ String DecimalToHex(int n){
+  String s = "";
+  int r = 3;
+  while(n>0){
+    r = n % 16;
+    if(r<=9){
+      String temp = "$r";
+      s = "${s}"+"${temp}";
+    }
+    else{
+      if(r == 10) s = s+"A";
+      else if(r == 11)  s = "${s}"+"B";
+      else if(r == 12)  s = "${s}"+"C";
+      else if(r == 13)  s = "${s}"+"D";
+      else if(r == 14)  s = "${s}"+"E";
+      else if(r == 15)  s = "${s}"+"F";
+    }
+    n = n~/16;
+  }
+  s = reverseString(s);
+  return s;
+ }
+ String DecimalToBinary(int n){
+  String binary = "";
+  int r = 0;
+  while(n>0){
+    r = n%2;
+    if(r%2==0) binary = "${binary}" + "0";
+    else binary = "${binary}" + "1";
+    n = n ~/ 2;
+  }
+  String reverseString(String input) {
+    var chars = input.split('');
+    return chars.reversed.join();
+  }
+  binary = reverseString(binary);
+  return binary;
+ }
 }
