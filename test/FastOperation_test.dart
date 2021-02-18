a79a98590490d0ca797a370bc1882055996958b6
shantanu1k <57897297+shantanu1k@users.noreply.github.com>
2021-02-18 15:54:12 +0530

    added distance converters

--- test/FastOperation_test.dart
+++ test/FastOperation_test.dart
@@ -5,8 +5,8 @@ import 'package:FastOperation/FastOperation.dart';
 void main() {
   test('adds one to input values', () {
     final fastOp = FastOperation();
-    expect(fastOp.SumNum([3,1.0,2.3,3,7.6,5,4,6]), 31.9);
-    expect(fastOp.MultiplyNum(3,2.1), 6.300000000000001);
-    expect(fastOp.DivideNum(6.3,9), 0.7);
+    expect(fastOp.sumNum([3,1.0,2.3,3,7.6,5,4,6]), 31.9);
+    expect(fastOp.multiplyNum(3,2.1), 6.300000000000001);
+    expect(fastOp.divideNum(6.3,9), 0.7);
   });
 }
