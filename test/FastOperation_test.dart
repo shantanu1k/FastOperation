f7f50bb6660f644f552de5534cbfd81292d65e34
shantanu1k <57897297+shantanu1k@users.noreply.github.com>
2021-02-21 11:25:27 +0530

    Organized functions

--- test/FastOperation_test.dart
+++ test/FastOperation_test.dart
@@ -4,9 +4,9 @@ import 'package:FastOperation/FastOperation.dart';
 
 void main() {
   test('adds one to input values', () {
-    final fastOp = FastOperation();
-    expect(fastOp.sumNum([3,1.0,2.3,3,7.6,5,4,6]), 31.9);
-    expect(fastOp.multiplyNum(3,2.1), 6.300000000000001);
-    expect(fastOp.divideNum(6.3,9), 0.7);
+    final distanceTest = distance();
+    expect(distanceTest.centToInch(231.323), 91.072096423);
+    expect(distanceTest.inchTokilo(3435221.1), 85.8805275);
+    expect(distanceTest.miliToInch(645.321), 25.4063523021);
   });
 }
